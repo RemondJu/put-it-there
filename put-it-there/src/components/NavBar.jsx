b5196598ac8e126bef3be367a3e368b05ced79fe
RemondJu <remond.neiluj@gmail.com>
2019-04-22 18:53:18 +0200

    NavBar implemented with spaced NavLinks

--- put-it-there/src/components/NavBar.jsx
+++ put-it-there/src/components/NavBar.jsx
@@ -3,24 +3,39 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import { NavLink } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
 
 const styles = {
   root: {
     flexGrow: 1,
-  },
+  }
 };
 
 const NavBar = (props) => {
   const { classes } = props;
-  
+
   return ( 
     <div className={classes.root}>
       <AppBar position="static" color="default">
         <Toolbar>
-          <Typography variant="h6" color="inherit">
-            Test
-          </Typography>
+          <Grid container spacing={24}>
+            <Grid item xs align="center">
+              <NavLink to="/">Link 1</NavLink>
+            </Grid>
+            <Grid item xs align="center">
+              <NavLink to="/">Link 2</NavLink>
+            </Grid>
+            <Grid item xs align="center">
+              <NavLink to="/">Put it there</NavLink>
+            </Grid>
+            <Grid item xs align="center">
+              <NavLink to="/">Link 3</NavLink>
+            </Grid>
+            <Grid item xs align="center">
+              <NavLink to="/">Link 4</NavLink>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     </div>
