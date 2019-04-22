0526bba15d5df6ce04d4732de6b85122e5285016
RemondJu <remond.neiluj@gmail.com>
2019-04-22 17:58:35 +0200

    integrated NavBar component in App

--- put-it-there/src/App.js
+++ put-it-there/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './views/HomePage'
 import './App.css';
+import NavBar from './components/NavBar';
 
 const App = () => {
     return (
       <div className="App">
+        <NavBar />
         <Switch>
           <Route exact path="/" component={HomePage} />
         </Switch>
